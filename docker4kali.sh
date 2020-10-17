6aa06fdcaa34ded9159008069b25d11e8461a322
HuskyHacks <57866415+HuskyHacks@users.noreply.github.com>
2020-10-17 16:58:45 -0400

    Update docker4kali.sh

--- docker4kali.sh
+++ docker4kali.sh
@@ -7,3 +7,5 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 echo deb [arch=amd64] https://download.docker.com/linux/debian stretch stable >> /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install docker-ce
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
