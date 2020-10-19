358a7d7f301689c41be5758683e5cdaf71306f4b
husky <huskyhacks@github.com>
2020-10-19 06:26:36 -0700

    updated versioned files

---
+++ sqldb/sql-scripts/CreateTables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(80),
+name VARCHAR(80),
+password VARCHAR(80));
+
+INSERT INTO users (username, name, password) VALUES ('admin', 'admin', 'D0ntH4km33PLZZZ');
+INSERT INTO users (username, name, password) VALUES ('datboi', 'alex', 'Y29uZ3JhdHMh');
