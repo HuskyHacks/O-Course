3b88871300650e053039989eff371059ab54e958
husky <huskyhacks@github.com>
2020-10-26 14:03:03 -0700

    adding xss

---
+++ www/vulns/xss/welcome.php
@@ -0,0 +1,82 @@
+<html>
+<head>
+    <title>Cross-Site Scripting (XSS)</title>
+</head>
+<body>
+
+<!-- A simple form that when submitted will navigate back to the same page -->
+<form action="" method="GET">
+    <!-- Adds `?q=VALUE` to the URL when the form is submitted -->
+    <input type="text" name="q">
+    <input type="submit" value="Search">
+</form>
+
+<!-- The results of the search are inserted here -->
+<div id="results"></div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+
+        /*
+            Get the search query from the URL. For example:
+            http://xss-example-page.nowhere/?q=Searching+for+things
+        */
+        var q = getQueryParameter('q');
+
+        if (q) {
+
+            search(q, function(error, results) {
+                showQueryAndResults(q, results);
+            });
+        }
+    });
+
+    function search(q, callback) {
+
+        // Fake results. We don't actually searching anything.
+        var results = [
+            'Result #1',
+            'Result #2',
+            'Result #3'
+        ];
+
+        callback(null, results);
+    }
+
+    function showQueryAndResults(q, results) {
+
+        var resultsEl = document.querySelector('#results');
+        var html = '';
+
+        html += '<p>Your search query:</p>';
+        html += '<pre>' + q + '</pre>';
+        html += '<ul>';
+
+        for (var index = 0; index < results.length; index++) {
+            html += '<li>' + results[index] + '</li>';
+        }
+
+        html += '</ul>';
+
+        resultsEl.innerHTML = html;
+    }
+
+    function getQueryParameter(name) {
+
+        var pairs = window.location.search.substring(1).split('&');
+        var pair;
+
+        for (var index = 0; index < pairs.length; index++) {
+
+            pair = pairs[index].split('=');
+
+            if (decodeURIComponent(pair[0]) === name) {
+                return decodeURIComponent(pair[1]);
+            }
+        }
+
+        return false;
+    }
+</script>
+</body>
+</html>
