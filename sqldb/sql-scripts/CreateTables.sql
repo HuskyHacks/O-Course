e202731415dbfebc6e94f3906e56fbce2f14eacd
husky <huskyhacks@github.com>
2020-11-01 13:47:47 -0800

    api tuning

--- sqldb/sql-scripts/CreateTables.sql
+++ sqldb/sql-scripts/CreateTables.sql
@@ -8,5 +8,6 @@ password VARCHAR(80));
 CREATE TABLE `userInfo`(
 `user_id` varchar(6) NOT NULL,
 `phone` varchar(7) NOT NULL,
-`socialsecnumber` varchar(9) NOT NULL
+`socialsecnumber` varchar(9) NOT NULL,
+`description` varchar(40) NOT NULL;
  )
--- www/vulns/api/v1/monitoring/accounts/index.php
+++ www/vulns/api/v1/monitoring/accounts/index.php
@@ -1,7 +1,35 @@
 <?php
 header("Content-type: text/json");
-//API can be called ONlY in POST method
+
+$servername = "webDB";
+$username = "root";
+$password = "rootpassword";
+$dbname = "website";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $task = isset($_POST['accounts']) ? mysqli_real_escape_string($conn, $_POST['accounts']) :  "";
+	$sql = "SELECT * FROM `userInfo`;";
+	$get_data_query = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+		if(mysqli_num_rows($get_data_query)!=0){
+		$result = array();
+		while($r = mysqli_fetch_array($get_data_query)){
+			extract($r);
+			$result[] = array("UID" => $user_id, "Phone" => $phone, 'SSN' => $socialsecnumber, "Desc" => $description);
+		}
+		$json = array("status" => 1, "info" => $result);
+	}
+	else{
+		$json = array("status" => 0, "error" => "not found!");
+	}
+@mysqli_close($conn);
+// Set Content-type to JSON
+header('Content-type: application/json');
+echo json_encode($json);
+}
+
+//API can be called ONlY in POST method
+elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // receive json from url and deocode and store in data
     $data = json_decode(file_get_contents('php://input'), true);
     //check if received all neccessary json format using is array
@@ -15,10 +43,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         //check all variales value received are in INTEGER only others type NOT allowed such as float and string
         if(is_int($uid) && is_int($phone) && is_int($socialsecnumber)) {
             //MYSQL connection for POSTING JSON variable to database
-            $servername = "webDB";
-            $username = "root";
-            $password = "rootpassword";
-            $dbname = "website";
 
             // Create connection
             $conn = new mysqli($servername, $username, $password, $dbname);
@@ -27,7 +51,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 die("Connection failed: " . $conn->connect_error);
             }
 
-            $sql = "INSERT INTO user (user_id, phone, socialsecnumber )
+            $sql = "INSERT INTO 'userInfo' (user_id, phone, socialsecnumber )
 			VALUES ('$uid','$phone','$socialsecnumber')";
 
             if ($conn->query($sql) === TRUE) {
@@ -56,6 +80,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 }
 else
-    //For Handle GET , PUT and others  Request
     echo json_encode(array( 'API'=>'server is working fine.'),true);
 ?>
