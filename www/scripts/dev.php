f3b65f873e48d7cd4d58b70fd0b2897daecfaf4a
husky <huskyhacks@github.com>
2020-11-02 06:17:15 -0800

    easter eggs

---
+++ www/scripts/dev.php
@@ -0,0 +1,15 @@
+<?php
+
+//Hey, you found it!
+
+//This is an easter egg flag that shows you're good at enumerating the website! So bravo.
+
+//Or, you just looked through the source code on the github page. But still, that's a great skill to have! Enumerating the website by source code review is a fantastic way to find bugs.
+
+//In any case, a huge thank you goes out to HackerOne (Jenn and Ben, thank you guys!), and Tom from VetSec for giving me the opportunity to put this together. I had a blast and I hope you enjoyed and learned something.
+
+//Thank you for playing! Now get out there and Hack All the Things!
+
+//-HuskyHacks
+
+//flag{super_sleuth_skills!}
